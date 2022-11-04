f9d13fb2a55ca7ae23ac5ce2dd437a1f5a8b3ebf
Georgy <egorslabodich1@gmail.com>
2022-11-04 13:27:10 +0300

    add filter price

--- src/components/catalog/v-catalog.vue
+++ src/components/catalog/v-catalog.vue
@@ -5,12 +5,36 @@
         </router-link>
 
         <h1>Catalog</h1>
-        <v-select
-                :options="categories"
-                @select="sortByCatigories"
-                :selected="selected"
+        <div class="v-catalog__filters">
+            <v-select
+                    :options="categories"
+                    @select="sortByCatigories"
+                    :selected="selected"
+            />
+            <div class="v-catalog__range-slider">
+                <input
+                        type="range"
+                        min="10000"
+                        max="100000"
+                        step="1000"
+                        v-model.number="minPrice"
+                        @change="setRangeSlider"
+                >
+                <input
+                        type="range"
+                        min="10000"
+                        max="100000"
+                        step="1000"
+                        v-model.number="maxPrice"
+                        @change="setRangeSlider"
+                >
+            </div>
+            <div class="range-value">
+                <p>Min: {{minPrice}}</p>
+                <p>Max: {{maxPrice}}</p>
+            </div>
+        </div>
 
-        />
     </div>
     <div class="v-catalog__list">
         <v-catalog-item
@@ -42,7 +66,9 @@
                     {name: 'Xiaomi', value: 'xPh'}
                 ],
                 selected:'Все',
-                sortedProducts: []
+                sortedProducts: [],
+                minPrice:'0',
+                maxPrice:'100000'
             }
         },
         computed: {
@@ -64,14 +90,26 @@
               'GET_PRODUCTS_FROM_API',
               'ADD_TO_CART'
           ]),
+            setRangeSlider(){
+              if(this.minPrice >this.maxPrice){
+                  let tmp = this.maxPrice
+                  this.maxPrice = this.minPrice
+                  this.minPrice = tmp
+              }
+              this.sortByCatigories()
+            },
             sortByCatigories(category){
-                this.sortedProducts = []
-                let vm= this
-                this.PRODUCTS.map(function (item){
-                    if(item.category === category.name){
-                        vm.sortedProducts.push(item)
-                    }
+                let vm = this
+                this.sortedProducts = [...this.PRODUCTS]
+                this.sortedProducts = this.sortedProducts.filter(function (item) {
+                    return item.price >= vm.minPrice && item.price <= vm.maxPrice
                 })
+                if(category){
+                    this.sortedProducts = this.sortedProducts.filter(function (e) {
+                        vm.selected === category.name
+                        return e.category === category.name
+                    })
+                }
                 this.selected = category.name
             },
             addToCart(data){
@@ -102,5 +140,30 @@
             padding: $padding*2;
             border: solid 1px #aeaeae;
         }
+        &__filters{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        &__range-slider{
+            width: 200px;
+            margin: auto 16px;
+            text-align: center;
+            position: relative;
+
+        }
+        &__range-slider svg, &__range-slider input[type=range]{
+            position: absolute;
+            left: 0;
+            bottom: 0;
+        }
+        input[type=range]::-webkit-slider-thumb {
+            z-index: 2;
+            position: relative;
+            top: 2px;
+            margin-top: -7px;
+        }
+
     }
 </style>
